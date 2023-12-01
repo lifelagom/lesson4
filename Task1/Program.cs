175edc9be9cecf5b6ceb32a6c7a81ab36c2e4f8c
Vladimir <buyanov.vv@yandex.ru>
2023-12-02 01:56:10 +0700

    сохранил код 1 задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,3 +2,37 @@
 // Программа завершается при вводе символа ‘q’ или при вводе числа,
 // сумма цифр которого чётная.
 
+string ReadStr(string text) {
+    System.Console.Write(text);
+    return Console.ReadLine();
+}
+// проверка суммы цифр на чётность
+bool CheckSumChet(string temp) {
+    bool end = false;
+    int sum = 0;
+    for(int i = 0; i<temp.Length; i++) {
+        sum += Convert.ToInt32(temp[i]);
+    }
+    if (sum%2==0) {
+        end = true;
+    }    
+    return end;
+}
+// проверка ввода символа ‘q’
+bool CheckEnd(string temp) {
+    bool end = true;
+    if ((temp == "q")||(CheckSumChet(temp))) {
+        end = false;
+    }
+    return end;
+}
+
+// основная программа
+string text = string.Empty;
+string temp;
+do {
+    temp = ReadStr("Введите целое число: ");
+    text += temp+" ";
+} while (CheckEnd(temp));
+text += "[STOP]";
+Console.WriteLine(text);
